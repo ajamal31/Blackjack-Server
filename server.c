49fc69cc6fe3bd237fc3aefa5769867c33fc95d5
jamala6 <jamala6@mymacewan.ca>
2017-10-23 15:19:26 -0600

    Add options for reusing addresses (setsockopt) and Bind the socket

--- server.c
+++ server.c
@@ -8,20 +8,22 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #define DEFAULT_PORT "4420"
 
 int get_socket()
 {
 	struct addrinfo hints, *results, *cur;
+	int socketfd;
 
 	// Initialize hints
 	memset(&hints, 0, sizeof(hints));
 	// Set options for a UDP server
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_DGRAM;
-        hints.ai_protocol = IPPROTO_UDP;
-        hints.ai_flags = AI_PASSIVE;
+	hints.ai_protocol = IPPROTO_UDP;
+	hints.ai_flags = AI_PASSIVE;
 
 	// Get avaliable addresses
 	int return_value = getaddrinfo(NULL, DEFAULT_PORT, &hints, &results);
@@ -34,9 +36,37 @@ int get_socket()
 	}
 
 	for (cur = results; cur != NULL; cur = cur->ai_next) {
+
+		// Open a socket
+		socketfd =
+		    socket(cur->ai_family, cur->ai_socktype, cur->ai_protocol);
+		if (socketfd == -1) {
+			perror("Socket fail");
+			continue;
+		}
+
+		int val = -1;
+
+		// Set options for reusing an address
+		return_value = setsockopt(socketfd, SOL_SOCKET, SO_REUSEADDR,
+					  &val, sizeof(val));
+		if (return_value == -1) {
+			perror("setsockopt");
+			close(socketfd);
+			continue;
+		}
+
+		// Bind the socket to an address
+		return_value = bind(socketfd, cur->ai_addr, cur->ai_addrlen);
+
+		if (return_value == -1) {
+			perror("Bind fail");
+			continue;
+		}
 		printf("family: %x\n", cur->ai_family);
-                printf("socktype: %x\n", cur->ai_socktype);
+		printf("socktype: %x\n", cur->ai_socktype);
+		break;
 	}
 
-	return 0;
+	return socketfd;
 }
