1e3beaaaa55e4d3961d0c3f9e1874389a0abf2a2
jamala6 <jamala6@mymacewan.ca>
2017-10-31 18:46:01 -0600

    Add headers to test something

--- blackjack.c
+++ blackjack.c
@@ -3,9 +3,10 @@
  * Assign: 2
  */
 
-#include <stdio.h>
-#include <stdint.h> // need to probably remove this header after, used for uint32_t;
 #include "server.h"
+#include <arpa/inet.h> //this will probably need to remove this, used for htonl(uint32_t)
+#include <stdint.h> // need to probably remove this header after, used for uint32_t;
+#include <stdio.h>
 
 int main(int argc, char const *argv[])
 {
@@ -13,7 +14,7 @@ int main(int argc, char const *argv[])
 	if ((socketfd = get_socket()) != 2) {
 		open_connection(socketfd);
 	} else {
-		fprintf(stderr, "%s\n", "Error opening a socket" );
+		fprintf(stderr, "%s\n", "Error opening a socket");
 		return 1;
 	}
 	return 0;
