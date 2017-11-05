9b087ca5fc6174f544fd0fb060eb162d6785206c
jamala6 <jamala6@mymacewan.ca>
2017-11-04 23:42:31 -0600

    add getopt

--- blackjack.c
+++ blackjack.c
@@ -7,11 +7,24 @@
 #include <arpa/inet.h> //this will probably need to remove this, used for htonl(uint32_t)
 #include <stdint.h> // need to probably remove this header after, used for uint32_t;
 #include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char const *argv[])
+int main(int argc, char *argv[])
 {
+	int c;
+	char * port = NULL;
+	while ((c = getopt(argc, argv, "p:")) != -1) {
+		switch (c) {
+		case 'p':
+			port = optarg;
+			break;
+		}
+	}
+
+	printf("port: %s\n", port);
+
 	int socketfd;
-	if ((socketfd = get_socket()) != 2) {
+	if ((socketfd = get_socket(port)) != 2) {
 		open_connection(socketfd);
 	} else {
 		fprintf(stderr, "%s\n", "Error opening a socket");
